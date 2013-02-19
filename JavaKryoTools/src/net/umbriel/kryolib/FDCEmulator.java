ac925625183e00ded1a5bfb5af1bd899f5b1339a
daniel <djameson@gmail.com>
2013-02-19 21:25:13 +0000

    FDCEmulator class...

---
+++ JavaKryoTools/src/net/umbriel/kryolib/FDCEmulator.java
@@ -0,0 +1,28 @@
+package net.umbriel.kryolib;
+
+public class FDCEmulator {
+
+	/**
+	 * Emulate FDC and return bits.
+	 * Variable rotational speed to deal with CAV formats too.
+	 *  
+	 */
+	
+	
+	public FDCEmulator() {
+		
+	}
+	
+	public void setTrack(StreamTrack s) {
+		
+	}
+	
+	public void zeroIndex() {
+		
+	}
+	
+	public int nextBit() {
+		return 0;
+	}
+	
+}
